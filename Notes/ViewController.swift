68bf482a06653f43b1c09c76b7a64a0c46acd3b9
RadenAR <radenabdulrahman@gmail.com>
2020-04-21 11:09:53 -0400

    Initial Commit

---
+++ Notes/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Notes
+//
+//  Created by Raden Abdul Rahman on 4/21/20.
+//  Copyright © 2020 Raden. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
